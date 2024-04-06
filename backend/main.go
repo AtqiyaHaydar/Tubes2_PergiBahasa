37abc1482aa5c173d2cbc6fc9b92cf9ddb93b206
AtqiyaHaydar <ashford1524@gmail.com>
2024-04-06 15:47:50 +0700

    feat: go api

--- backend/main.go
+++ backend/main.go
@@ -2,34 +2,58 @@ package main
 
 import (
 	"fmt"
-
+	"encoding/json"
 	"github.com/gocolly/colly"
+	"net/http"
+	"log"
 )
 
 func main() {
-	result := scrape("pertamina") // hanya contoh
-	for i := 0; i < len(result); i++ {
-		fmt.Println(result[i])
-	}
+	http.HandleFunc("/scrape", handleScrape)
+	fmt.Println("server listening on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+func handleScrape(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "OPTIONS" {
+		// Respond to preflight requests
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // Allow requests from this origin
+		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	// Handle GET request for data scraping
+	query := r.URL.Query().Get("query")
+	if query == "" {
+		http.Error(w, "Query parameter is required", http.StatusBadRequest)
+		return
+	}
+
+	result := scrape(query)
+	responseJSON, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // Allow requests from this origin
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseJSON)
+}
+
+
 func scrape(articleName string) []string {
-
 	var links []string
-
-	// creating a new Colly instance
 	c := colly.NewCollector()
-
-	// visiting the target page
 	c.OnHTML("div#mw-content-text a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
 		if link[:6] == "/wiki/" {
 			links = append(links, link[6:])
 		}
-
 	})
-
 	c.Visit("https://en.wikipedia.org/wiki/" + articleName)
-
 	return links
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,28 @@
+"use client"
+
+import { useState } from "react";
+import axios from "axios";
+
 import { Input } from "@/components/ui/input";
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const [query, setQuery] = useState<string>("");
+  const [result, setResult] = useState<string[]>([]);
+
+  const handleSearch = async () => {
+    console.log("CLIKED!")
+    console.log("Query:", query)
+
+    try {
+      const response = await axios.get(`http://localhost:8080/scrape?query=${query}`)
+      console.log("Response:", response.data)
+      setResult(response.data)
+    } catch (error) {
+      console.log('Error fetching data:', error)
+    }
+  }
+  
   return (
     <main className="w-full h-screen  flex flex-col items-center justify-center gap-y-12">
       <div className="space-y-4">
@@ -9,9 +30,20 @@ export default function Home() {
         <p className="text-white">Pemanfaatan Algoritma IDS dan BDS dalam Permainan WikiRace</p>
       </div>
       <Input 
+        type="text"
         className="max-w-[500px] z-[20]"
         placeholder="Masukkan Link Wikipedia"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
+      <Button className="z-[20]" onClick={handleSearch}>Search</Button>
+      <ul className="grid grid-cols-3 gap-x-9 gap-y-1 text-center">
+        {result.slice(0, 50).map((link: string, index: number) => (
+          <li key={index}>
+            <p className="text-white">{link}</p>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
