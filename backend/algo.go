89fedd7aef7e19f2fcbc4a646be9bb0286d10b19
Venantius Sean Ardi Nugroho <seanardi4@gmail.com>
2024-04-06 16:50:30 +0700

    struct

--- backend/algo.go
+++ backend/algo.go
@@ -7,13 +7,26 @@ type Node struct {
 	depth int
 }
 
-// nanti diganti links dengan implementasi scrapper
-func makeNode(start string, links []string) *Node {
+func makeNode(start string, links []string, _depth int, _trail []string) *Node {
 	return &Node{
 		link:  links,
-		depth: 0,
+		depth: _depth,
 		name:  start,
+		trail: _trail,
 	}
+}
+
+// nanti diganti links dengan implementasi scrapper
+func makeChildren(start Node) []Node {
+	var tempStr []string = scrape(start.name)
+	var result []Node
+	for i := 0; i < len(tempStr); i++ {
+		var tempNode Node
+		var container []string
+		tempNode = *makeNode(tempStr[i], container, start.depth+1, append(start.trail, start.name))
+		result = append(result, tempNode)
+	}
+	return result
 }
 
 // func IDS(Node actor,name target, maxdepth int) []Node{
