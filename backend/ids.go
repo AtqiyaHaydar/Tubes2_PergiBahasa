5f8bccc177fdc66b6abc05b6362f9b0661b594db
BiZaRrE96 <91012945+BiZaRrE96@users.noreply.github.com>
2024-04-23 15:33:44 +0700

    store visits information

--- backend/ids.go
+++ backend/ids.go
@@ -13,20 +13,35 @@ func hasJank(text string) bool {
 	}
 }
 
-func IDS(actor Node, target string, maxdepth int) []Node {
+var visits int = 0
+
+func IDS(actor Node, target string, maxdepth int, firstonly bool) []Node {
+	visits++
 	//define
 	var retval []Node //rerturn value
 	//check if last or found
 	if actor.name == target {
 		if actor.name == target {
 			retval = append(retval, actor)
-			fmt.Println(actor)
+			fmt.Println("Found", target, "in", actor.depth, "by", actor.trail)
+			if firstonly == true {
+				return retval
+			}
 		}
 	} else if actor.depth < maxdepth { //seek
 		//for each link in actor.link
 		for i := 0; i < len(actor.link); i++ {
 			//fmt.Println("Current[", actor.depth+1, "] : ", actor.link[i])
 			//fmt.Printf("Current[%d] : %s ", actor.depth+1, actor.link[i])
+
+			//skip if link has been visitted before within self history to prevent useless looping
+			for _, oldlink := range actor.trail {
+				if actor.link[i] == oldlink {
+					//fmt.Printf("%s == %s, skipping\n", actor.link[i], oldlink)
+					continue
+				}
+			}
+
 			if !hasJank(actor.link[i]) {
 				var child Node = *makeNode(actor.link[i], []string{})
 				child.depth = actor.depth + 1
@@ -36,7 +51,10 @@ func IDS(actor Node, target string, maxdepth int) []Node {
 					//fmt.Printf("|| Scrapped!")
 				}
 				// Itterate over result of IDS, if empty then stop
-				for _, item := range IDS(child, target, maxdepth) {
+				for _, item := range IDS(child, target, maxdepth, firstonly) {
+					if firstonly == true {
+						return append(retval, item)
+					}
 					retval = append(retval, item)
 				}
 			} else {
